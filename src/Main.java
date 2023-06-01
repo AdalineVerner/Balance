a952b7af22432ca2d26b9283ebbbaedcf3143418
Xenia <xzinnatullina@mail.ru>
2023-06-01 19:03:07 +0300

    scr changed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int balance = 300;
+        int refill = 2000;
+        int bonus;
+        int total;
+
+        if (refill < 1000) {
+            bonus = 0;
+            System.out.println(bonus);
+        } else {
+            bonus = refill / 100;
+            System.out.println(bonus);
+        }
+
+        System.out.println(total = balance + refill + bonus);
     }
 }
